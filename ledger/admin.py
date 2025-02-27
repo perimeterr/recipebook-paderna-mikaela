430c91e90789d8e1c70a78ef6dffced08f377537
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-02-27 16:49:52 +0800

    Added admin page

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline,]
+
+
+admin.site.register(Recipe, RecipeAdmin)
