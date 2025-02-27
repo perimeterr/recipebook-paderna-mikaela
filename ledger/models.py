909746a7a09eee7091af0fa72c61a8339c9409be
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-02-27 14:56:04 +0800

    Created models for Ingredient, Recipe, and RecipeIngredient

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.charField(max_length=100)
+
+
+class Recipe(models.Model):
+    name = models.charField(max_length=100)
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField
+    ingredient = models.ForeignKey(Ingredient)
+    recipe = models.ForeignKey(Recipe)
