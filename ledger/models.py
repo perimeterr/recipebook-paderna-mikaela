b5e215a226c50ebb77210ecbf1261f59237ab576
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-02-27 15:25:58 +0800

    Set string representation and absolute url of Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -4,12 +4,31 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.charField(max_length=100)
 
+    def __str__(self):
+        return f'{self.name}'
+    
 
 class Recipe(models.Model):
     name = models.charField(max_length=100)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[self.id])
+
 
 class RecipeIngredient(models.Model):
     quantity = models.IntegerField
-    ingredient = models.ForeignKey(Ingredient)
-    recipe = models.ForeignKey(Recipe)
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete = models.CASCADE,
+        default = 1,
+        related_name='recipe'
+        )
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete = models.CASCADE,
+        default = 1,
+        related_name='ingredients'
+        )
--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from .views import recipes, recipe1, recipe2
 
+
 urlpatterns = [
-    path('recipes/list', recipes, name='list'),
+    path('recipes/list', recipes, name='recipe_list'),
     path('recipe/1', recipe1, name='1'),
     path('recipe/2', recipe2, name='2')
 ]
