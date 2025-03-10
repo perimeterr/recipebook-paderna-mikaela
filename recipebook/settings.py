d0ea66b02fbb8c5c8630e715fb8dd89745bac74c
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-10 22:07:30 +0800

    Modified logout redirect url in settings

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/recipes/list'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGOUT_REDIRECT_URL = '/recipes/list'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
