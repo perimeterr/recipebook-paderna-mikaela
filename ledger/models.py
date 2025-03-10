007e955f660e53d1af3c5b51795cde6b969b27bb
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-10 22:01:56 +0800

    Modfiied Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -14,6 +14,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name}'
@@ -36,3 +39,4 @@ class RecipeIngredient(models.Model):
         default = 1,
         related_name='ingredients'
         )
+    
