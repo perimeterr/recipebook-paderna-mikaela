4ffeff80064d77203ccaef0a329544fa8c82f490
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-10 22:06:16 +0800

    Added admin panels for Ingredient model

--- ledger/admin.py
+++ ledger/admin.py
@@ -7,9 +7,14 @@ class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [RecipeIngredientInline,]
 
 
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
