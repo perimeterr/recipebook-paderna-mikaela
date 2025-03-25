212fcf266d94fc0a3a8b3f10e539b9ad93ec7251
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-25 11:04:58 +0800

    Edited settings to include media, added url patterns to project-level urls for the media

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -126,8 +126,10 @@ STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 LOGIN_REDIRECT_URL = '/recipes/list'
-LOGOUT_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
