a955a34496bc575dcd31ecf876181b10e5cf1762
perimeterr <mikaela.paderna@student.ateneo.edu>
2025-03-25 11:03:45 +0800

    Created views for adding a new recipe and uploading images using forms

--- ledger/views.py
+++ ledger/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-from .models import Recipe
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 
 class RecipeListView(ListView):
@@ -15,3 +18,21 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html'
     redirect_field_name = '/recipes/list'
+
+
+class AddRecipeView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipe_create.html'
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe_list')
+    
+
+class AddRecipeImageView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = 'recipe_add_image.html'
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe_detail', kwargs={ 'pk': self.object.recipe.pk })
